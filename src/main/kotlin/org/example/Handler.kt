a721e4eaa799c7f2922e8d498e6ac466b4be934e
YCX-Home <ycx974577817@gmail.com>
2023-02-06 22:33:54 +0800

    3

--- src/main/kotlin/org/example/Handler.kt
+++ src/main/kotlin/org/example/Handler.kt
@@ -1,3 +1,5 @@
+package org.example
+
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.S3Event
